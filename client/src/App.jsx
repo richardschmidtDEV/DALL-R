00dd9a1cd7ef1969a2f0c5c3a3e1ae618087d956
richardschmidtDEV <RichardSchmidtDEV@outlook.com>
2023-01-30 17:27:43 +0100

    V2

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,18 +2,22 @@ import React from 'react';
 import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 
 import { logo } from './assets';
+
 import { Home, CreatePost } from './pages';
 
 const App = () => (
+
     <BrowserRouter>
-        <header className="w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]">
+        <hader className="w-full flex justify-between items-center bg-zinc-900  sm:px-8 px-4 py-4 border-b border-slate-500">
             <Link to="/">
                 <img src={logo} alt="logo" className="w-28 object-contain" />
+
+
             </Link>
 
             <Link to="/create-post" className="font-inter font-medium bg-[#6469ff] text-white px-4 py-2 rounded-md">Create</Link>
-        </header>
-        <main className="sm:p-8 px-4 py-8 w-full bg-[#f9fafe] min-h-[calc(100vh-73px)]">
+        </hader>
+        <main className="sm:p-8 px-4 py-8 w-full bg-slate-900 min-h-[calc(100vh-73px)]">
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/create-post" element={<CreatePost />} />
